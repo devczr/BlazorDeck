23f3d656e1870cce117f3a9763ddf160354576c9
devczr <czr@devczr.com>
2023-01-18 19:56:52 -0700

    adds model types

---
+++ Shared/Card.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorDeck.Shared;
+
+public class Card
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public Deck? Deck { get; set; }
+    public int DeckId { get; set; }
+
+}
---
+++ Shared/Deck.cs
@@ -0,0 +1,8 @@
+﻿
+namespace BlazorDeck.Shared;
+
+public class Deck
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
