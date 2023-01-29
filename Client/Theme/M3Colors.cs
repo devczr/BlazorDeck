b364c14781fe2c91f9d635c1fd6c67884eb126f3
devczr <czr@devczr.com>
2023-01-29 12:55:02 -0700

    adds custom theme
    Colors based on Material Design 3

---
+++ Client/Theme/M3Colors.cs
@@ -0,0 +1,40 @@
+﻿using MudBlazor;
+
+namespace BlazorDeck.Client.Theme;
+
+public static class M3Colors
+{
+    public static class Dark
+    {
+        public const string primary = "#FFAEDB";
+        public const string onPrimary = "#5A1144";
+        public const string primaryContainer = "#762A5C";
+        public const string onPrimaryContainer = "#FFD8EB";
+        public const string secondary = "#DEBECD";
+        public const string onSecondary = "#402A36";
+        public const string secondaryContainer = "#58404C";
+        public const string onSecondaryContainer = "#FCD9E9";
+        public const string tertiary = "#F3BA9D";
+        public const string onTertiary = "#4A2713";
+        public const string tertiaryContainer = "#653D27";
+        public const string onTertiaryContainer = "#FFDBCA";
+        public const string error = "#FFB4AB";
+        public const string errorContainer = "#93000A";
+        public const string onError = "#690005";
+        public const string onErrorContainer = "#FFDAD6";
+        public const string background = "#1F1A1C";
+        public const string onBackground = "#EAE0E3";
+        public const string surface = "#1F1A1C";
+        public const string onSurface = "#EAE0E3";
+        public const string surfaceVariant = "#504349";
+        public const string onSurfaceVariant = "#D3C2C9";
+        public const string outline = "#9C8D93";
+        public const string inverseOnSurface = "#1F1A1C";
+        public const string inverseSurface = "#EAE0E3";
+        public const string inversePrimary = "#924274";
+        public const string shadow = "#000000";
+        public const string surfaceTint = "#FFAEDB";
+        public const string surfaceTintColor = "#FFAEDB";
+    }
+
+}
